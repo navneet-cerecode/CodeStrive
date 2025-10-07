1ba28238b10fb53ff9153ba932e211ed231f8d31
navneet-cerecode <cerecode9@gmail.com>
2025-10-08 05:02:02 +0530

    Add files via upload

---
+++ UNIT1_Hashing/Hashing.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for(int i = 0 ; i < n ; i++){
+        cin >> arr[i];
+    }
+
+    //PRECOMPUTE
+
+    map<int,int> mpp;
+    for(int i =0; i< n ; i++){
+        mpp[arr[i]]+=1;
+    }
+
+    int q;
+    cin >> q;
+    while(q--){
+        int number;
+        cin >> number;
+
+        cout << mpp[number] << endl;
+
+    }
+    return 0;
+
+}
